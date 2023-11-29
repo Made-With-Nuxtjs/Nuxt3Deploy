bd303c3a5da12849bed21076e980c4bb1cf5ef82
ICT Layer <32593898+ICTLAYER@users.noreply.github.com>
2023-11-29 23:40:58 +0600

    Update login_successful.vue

--- pages/login_successful.vue
+++ pages/login_successful.vue
@@ -2,12 +2,6 @@
     <div>
         <h2>Login Successfull</h2>
         <br>
-        <nuxt-link to="/apiparty">Api Party</nuxt-link>
-        <br>
-        <nuxt-link to="/serverapi_dollar">Server API Dollar</nuxt-link>
-        <br>
-        <nuxt-link to="/">Index</nuxt-link>
-        <br>
-        <nuxt-link to="/apiparty_useFetch">Api Party Use Fetch</nuxt-link>
+        <nuxt-link to="/">Back To Index</nuxt-link>
     </div>
 </template>
