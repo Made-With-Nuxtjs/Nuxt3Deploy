a685876e363658b3567e3dd340b5a567ebad768f
ICT Layer <32593898+ICTLAYER@users.noreply.github.com>
2023-11-30 15:28:42 +0600

    UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["nuxt-api-party"],
+  modules: ["nuxt-api-party", "@nuxt/ui"],
 
   apiParty: {
     endpoints: {
